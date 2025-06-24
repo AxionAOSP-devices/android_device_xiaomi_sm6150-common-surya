44929880d4a2ba841958db59a4e8cf4372756834
HinohArata <mmgcntk@gmail.com>
2025-06-25 01:46:37 +0700

    sm6150-common: Disable casefolding for emulated storage without sdcardfs
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -22,9 +22,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/sm6150-common/sm6150-common-vendor.mk)
 
-# Enable project quotas and casefolding for emulated storage without sdcardfs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
